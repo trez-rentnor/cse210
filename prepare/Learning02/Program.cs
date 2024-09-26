428d5ca792934a1202f58a77fd760ce4f9d43894
Randy Jones <randy910@gmail.com>
2024-09-26 07:59:10 -0700

    Add Learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "SW Engineer";
+        job1._startYear = 2012;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "SW Manager";
+        job2._startYear = 2020;
+        job2._endYear = 2024;
+
+        Resume resume = new Resume();
+        resume._name = "Allison Rose";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
